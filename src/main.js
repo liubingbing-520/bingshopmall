3126a0dea6c99687474a3123392e83fd8af41324
liubingbing <2993171923@qq.com>
2021-04-27 22:35:51 +0800

    bingbing

--- src/main.js
+++ src/main.js
@@ -6,3 +6,22 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// 添加git文件
+// git init
+
+// 创建vue.config.js
+// git add . （添加至缓存区）
+// git commit -m 'xxx' （提交） -m代表注释
+
+// git status
+// 查看当前状态（被修改的文件）
+
+// git push 提交到服务器
+
+// person acess token
+// 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
+
+// git remote rm origin
+// git remote add origin https://github.com/liubingbing-520/bingshopmall.git
+// git push -u origin master
